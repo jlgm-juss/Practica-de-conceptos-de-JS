2b85e40bfb654655f54719988576a81e55cb8b1c
Jose Luis <juss.jlgm@gmail.com>
2022-07-02 22:51:52 -0300

    ejercicio de suma con JS

--- JS/03-practica.js
+++ JS/03-practica.js
@@ -1,15 +1,1 @@
-// scope de Javascript
-
-//tipos de scope
-
-//global
-
-let persona = {
-  nombre: "jose",
-  direccion: "Av. Jaldo 800",
-};
-
-const getDatos = function () {
-  console.log("nombre: ${persona.nombre}");
-  console.log("direccion: ${persona.direccion}");
-};
+document.write('resultado : '+ (5 + 8))
