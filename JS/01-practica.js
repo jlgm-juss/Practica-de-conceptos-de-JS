232ceaa34a8f8ae05aa7cfe7e52ce5e42c7b5d48
Jose Luis <juss.jlgm@gmail.com>
2022-07-02 22:46:05 -0300

    agregue h1 con Hello Wordl

--- JS/01-practica.js
+++ JS/01-practica.js
@@ -1,12 +1,3 @@
-/*let numero = 60;
-
-if (numero === 10) {
-  console.log("el número 60 es igual a 10");
-} else if (numero > 10) {
-  console.log("el número 60 es mayor a 10");
-} else {
-  console.log("el número 60 es menor a 10");
-}*/
 
 alert('practicando JS')
 
